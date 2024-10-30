a6a9e0bcf0a23caaf01b392d8cc134f23fde07cd
Heath Provost <heath_provost@stuller.com>
2024-10-30 14:34:04 -0500

    fix if syntax

--- devbox.sh
+++ devbox.sh
@@ -112,7 +112,7 @@ function print_as() {
   eval "color=\${${msgtype}_color}"
 
   # use sed to highlight quoted substrings in $2 and store as msg
-  local msg=$(echo -n -e "$(echo -e -n "$2" | sed -e "s/'\([^'\"]*\)'/\\${blue}\1\\${reset}\\${color}/g")")
+  local msg=$(echo -n -e "$(echo -e -n "$2" | sed -e "s/'\([^'\\\"]*\)'/\\${blue}\1\\${reset}\\${color}/g")")
 
   # for prompts dont emit a linebreak
   if [ "$msgtype" = "prompt" ]; then
